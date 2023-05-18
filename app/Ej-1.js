c9d2c67bf372fe613f1049ea72ff6818aba3f8a2
NachoDelfiner <delfinerignacio@gmail.com>
2023-05-18 15:02:32 -0300

    Ejercicios-1

---
+++ app/Ej-1.js
@@ -0,0 +1,49 @@
+// Escribir un programa que muestre un prompt donde el usuario ingresa un número,
+// luego muestra otro prompt pidiendo otro número, finalmente el programa muestra
+// una alerta con el resultado de la suma
+
+// function suma() {
+//   let num1 = parseInt(prompt("Ingrese el primer numero a sumar"));
+//   let num2 = parseInt(prompt("Ingrese el segundo numero a sumar"));
+
+//   let resul = num1 + num2;
+//   alert(resul);
+// }
+// suma();
+
+// -------------------------------------------------------------------------------
+
+// Crear un programa donde el usuario ingrese en un prompt la temperatura en Celcius
+// y el programa muestre en una alerta la temperatura en Fahrenheit
+
+// function temFahrenheit() {
+//   let cel = parseInt(prompt("Ingrese la temperatura en celcius"));
+
+//   const fahrenheit = (cel * 9) / 5 + 32;
+//   alert(fahrenheit);
+// }
+// temFahrenheit();
+
+// -------------------------------------------------------------------------------
+
+// Pedir un número a un usuario a través de un prompt y luego dividirlo por 10, ejemplo:
+// 5 / 10 igual 0.5
+
+// function pedirNum() {
+//   let num = parseInt(prompt("Ingrese el numero para dividirlo entre 10"));
+//   const resul = num / 10;
+//   alert(resul);
+// }
+// pedirNum();
+
+// -------------------------------------------------------------------------------
+
+// Si a = [1,2,3] crear un script que determine de muestre un alert del tipo de dato de a.
+
+// const a = [1, 2, 3];
+
+// for (let i = 0; i < a.length; i++) {
+//   console.log(typeof a[i]);
+// }
+
+// -------------------------------------------------------------------------------
