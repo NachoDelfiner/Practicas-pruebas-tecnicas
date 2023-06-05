d61b74ca49f57eb1bdf442377f2677b3d458158a
NachoDelfiner <delfinerignacio@gmail.com>
2023-06-05 17:13:22 -0300

    actualizacion

--- app/Ejercicios.js
+++ app/Ejercicios.js
@@ -176,18 +176,29 @@
 
 // Determinar si un año dado es bisiesto
 
-let anio = parseInt(prompt("Ingrese un año"));
+// let anio = parseInt(prompt("Ingrese un año"));
 
-if (anio === 365) {
-  console.log("Es un año comun");
-} else {
-  if (anio < 365) {
-    console.log("Es menos de un año");
-  } else {
-    if (anio > 366) {
-      console.log("Es mas de un año");
-    } else if (anio === 366) {
-      console.log("Es un año bisiesto");
-    }
-  }
-}
+// if (anio === 365) {
+//   console.log("Es un año comun");
+// } else {
+//   if (anio < 365) {
+//     console.log("Es menos de un año");
+//   } else {
+//     if (anio > 366) {
+//       console.log("Es mas de un año");
+//     } else if (anio === 366) {
+//       console.log("Es un año bisiesto");
+//     }
+//   }
+// }
+
+// -------------------------------------------------------------------------------
+
+// Escribir un programa JavaScript en el que el programa escoge al azar un entero entre
+// 1 y 10, el usuario , luego a el usuario se le pedirá que introduzca un número en un
+// popup para intentar adivinarlo. Si la entrada del usuario coincide con el número de
+// conjetura, el programa mostrará un mensaje de "buen trabajo" de lo contrario
+// mostrará un mensaje de "No corresponde"
+
+// const num = Math.round(Math.random());
+// console.log(num);
